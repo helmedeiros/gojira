676ad2f1468ad17df6b0750d55ccac94b163dfe5
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-09-26 09:45:08 -0700

    first index printing jira data

---
+++ index.js
@@ -0,0 +1,11 @@
+var request = require("request");
+var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=10926&swimlaneId=11138&from=2014-07-17&to=2019-09-10&os_username=tv_pas&os_password=tvuser";
+
+request(url, function(error, response, body) {
+
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("request", body);
+    }
+});
