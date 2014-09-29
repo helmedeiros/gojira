49581b4777fa7c4f8628e7259c9c735c2e7108dc
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-09-29 16:14:20 -0700

    using json-csv to parse the json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var csv = require('json-csv')
 var fs = require("fs");
 
 var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=10926&swimlaneId=11138&from=2014-07-17&to=2019-09-10&os_username=tv_pas&os_password=tvuser";
+var day = 60 * 60 * 24 * 1000;
 
 request(url, function(error, response, body) {
     if(error) {
@@ -11,24 +12,65 @@ request(url, function(error, response, body) {
         var json = JSON.parse(body);
         var issues = json.issues;
 
-        var output = "Key, Summary, Backlog, In Progress, Validation, Sign Off, Done, Cycle Time, Swimlane, Sign-off Date\n";
-
-        for (var x=0; x < issues.length; x++) {
-            output += issues[x].key + ", ";
-            output += issues[x].summary + ", ";
-            var times = issues[x].workingTime;
-            for (var y=0; y < times.length; y++) {
-                output += (times[y] / (60 * 60 * 24 * 1000)) + ", ";
+        csv.csvBuffered(issues, {
+            fields : [ {
+                    name : 'key',
+                    label : 'Key'
+                },{
+                    name : 'summary',
+                    label : 'Summary'
+                }, {
+                    name : 'workingTime.0',
+                    label : 'Backlog',
+                    filter : function(value) {
+                        return value / day;
+                    }
+                }, {
+                    name : 'workingTime.1',
+                    label : 'In Progress',
+                    filter : function(value) {
+                        return value / day;
+                    }
+                }, {
+                    name : 'workingTime.2',
+                    label : 'Validation',
+                    filter : function(value) {
+                        return value / day;
+                    }
+                }, {
+                    name : 'workingTime.3',
+                    label : 'Sign Off',
+                    filter : function(value) {
+                        return value / day;
+                    }
+                }, {
+                    name : 'workingTime.4',
+                    label : 'Done',
+                    filter : function(value) {
+                        return value / day;
+                    }
+                }, {
+                    name : 'workingTime',
+                    label : 'Lead Time',
+                    filter : function(value) {
+                        var lead_time = 0;
+                        for (var x=1; x < value.length - 1; x++) {
+                            lead_time += value[x];
+                        }
+                        return lead_time / day;
+                    }
+                }]
+            },
+            function(err, output) {
+                fs.writeFile("/tmp/test.csv", output, function(err) {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("The file was saved! /tmp/test.csv");
+                        console.log(output);
+                    }
+                });
             }
-            console.log(output);
-            output += "\n";
-        }
-        fs.writeFile("/tmp/test.csv", output, function(err) {
-            if(err) {
-                console.log(err);
-            } else {
-                console.log("The file was saved! /tmp/test.csv");
-            }
-        });
+        );
     }
 });
