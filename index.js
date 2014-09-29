cbf4f74f9fc823c0fc2421d4373aaadf698974ef
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-09-29 11:43:31 -0700

    saving csv to filesystem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require("request");
 var json2csv = require("json2csv");
+var fs = require("fs");
 
 var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=10926&swimlaneId=11138&from=2014-07-17&to=2019-09-10&os_username=tv_pas&os_password=tvuser";
 
@@ -11,8 +12,14 @@ request(url, function(error, response, body) {
         json2csv({data: jsonBody.issues, fields: ['key','summary']}, function(err, csv) {
             if (err)
                 console.log(err);
-            console.log(csv);
+
+            fs.writeFile("/tmp/test", csv, function(err) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    console.log("The file was saved! /tmp/test");
+                }
+            });
         });
-        console.log("request", jsonBody);
     }
 });
