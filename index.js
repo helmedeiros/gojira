f83e1bda608ae39e6edf1ef40fd0622085525350
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-09-29 11:39:10 -0700

    converting json to csv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var json2csv = require("json2csv");
 
 var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=10926&swimlaneId=11138&from=2014-07-17&to=2019-09-10&os_username=tv_pas&os_password=tvuser";
 
@@ -7,6 +8,11 @@ request(url, function(error, response, body) {
         console.log(error);
     } else {
         var jsonBody = JSON.parse(body);
+        json2csv({data: jsonBody.issues, fields: ['key','summary']}, function(err, csv) {
+            if (err)
+                console.log(err);
+            console.log(csv);
+        });
         console.log("request", jsonBody);
     }
 });
