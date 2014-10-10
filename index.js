41668c10272fdb7c8101f483b1dd99d45692ef8b
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-10-10 18:29:51 -0300

    getting issues details from jira search

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var request = require("request");
 var fs = require("fs");
 
-var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=10926&swimlaneId=11138&from=2014-07-17&to=2019-09-10&os_username=tv_pas&os_password=tvuser";
+var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=10926&swimlaneId=11138&from=2010-07-17&to=2019-10-10&os_username=tv_pas&os_password=tvuser";
 var day = 60 * 60 * 24 * 1000;
+var COMMA = ',';
 
 var save_to_file = function(file_name, content) {
     fs.writeFile(file_name, content, function(err) {
@@ -26,7 +27,22 @@ var lead_times = function(issue) {
     return  lead_time + "," + backlog + "," + in_progress + "," + validation + "," + sign_off + "," + done + ",";
 }
 
-request(url, function(error, response, body) {
+var issues_url = function() {
+    var AND = '+and+';
+    var url = 'https://jira.example.com/rest/api/2/search?';
+    url += 'jql=project=DEMO';
+    url += AND;
+    url += 'status=Done'
+    url += AND;
+    url += 'component="Example Component"';
+    url += AND;
+    url += '"Work Group"=Application';
+    url += '&maxResults=2';
+    url += '&os_username=tv_pas&os_password=tvuser';
+    return url;
+}
+
+request(issues_url(), function(error, response, body) {
     if(error) {
         console.log(error);
     } else {
@@ -37,13 +53,13 @@ request(url, function(error, response, body) {
 
         for (var x=0; x < issues.length; x++) {
             var issue = issues[x];
-            csv += "Type???,";
-            csv += issue.key + ",";
-            csv += "\"" + issue.summary + "\",";
-            csv += "Status???,";
-            csv += "Story Points???,";
-            csv += "Projected Lead Time???,";
-            csv += lead_times(issue);
+            csv += issue.fields.issuetype.name + COMMA;
+            csv += issue.key + COMMA;
+            csv += "\"" + issue.fields.summary + "\",";
+            csv += issue.fields.status.name + COMMA;
+            csv += issue.fields.customfield_10003 + COMMA;
+            csv += issue.fields.customfield_10003 * 1.25 + COMMA;
+            csv += '';  //lead_times(issue);
             csv += "Over Under???,";
             csv += "Over Under %???";
             csv += "\n";
