302a1ad6b6491da48f230d6e59c33d0ab2e39968
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-10-22 15:11:27 -0200

    adding helmet soup swim lane

--- index.js
+++ index.js
@@ -68,7 +68,20 @@ var issues_url = function() {
 }
 var durations = [];
 
-var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=10450&swimlaneId=11263&swimlaneId=11138&swimlaneId=10926&from=2014-07-17&to=2015-09-10&os_username=tv_pas&os_password=tvuser";
+
+swimlane=11466
+swimlane=10450
+swimlane=10926
+swimlane=11263
+swimlane=11138
+
+swimlaneId=11466
+swimlaneId=10450
+swimlaneId=10926
+swimlaneId=11263
+swimlaneId=1113
+
+var url = "https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=11466&swimlaneId=10450&swimlaneId=10926&swimlaneId=11263&swimlaneId=11138&from=2014-07-17&to=2015-09-10&os_username=tv_pas&os_password=tvuser";
 request(url, function(error, response, body) {
     if(error) {
         console.log(error);
