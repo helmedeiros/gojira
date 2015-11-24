36f614a9c86dddd8cad1e419b71ea5136796681f
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-11-24 14:08:25 -0200

    solve issue getting durations

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ var populate_times = function(line, issues, issue_key, csv_columns) {
     }
 };
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var durations = [];
 
 var issues_url = GOJIRA.url.issues(GOJIRA.config.project_key, GOJIRA.config.component,
@@ -52,7 +54,6 @@ var control_chart_url = GOJIRA.url.control_chart(GOJIRA.config.control_chart,
     GOJIRA.config.user,
     GOJIRA.config.password, GOJIRA.config.from, GOJIRA.config.to);
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 request(control_chart_url, function(error, response, body) {
     if (error) {
         console.log(error);
@@ -60,42 +61,43 @@ request(control_chart_url, function(error, response, body) {
         var json = JSON.parse(body);
         durations = json.issues;
     }
-}).pipe(
-    request(issues_url, function(error, response, body) {
-        if (error) {
-            console.log(error);
+});
+
+
+request(issues_url, function(error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        var json = JSON.parse(body);
+        var issues = json.issues;
+
+        if (!issues) {
+            console.error(
+                'No issues returned. Please check your project, component and work group settings.\n'
+            )
         } else {
-            var json = JSON.parse(body);
-            var issues = json.issues;
 
-            if (!issues) {
-                console.error(
-                    'No issues returned. Please check your project, component and work group settings.\n'
-                )
-            } else {
+            var csv = GOJIRA.csv.header(GOJIRA.config.csv_header_columns);
 
-                var csv = GOJIRA.csv.header(GOJIRA.config.csv_header_columns);
+            for (var x = 0; x < issues.length; x++) {
+                var issue = issues[x];
+                var issue_line = function() {};
+                issue_line.type = issue.fields.issuetype.name;
+                issue_line.key = issue.key;
+                issue_line.summary = "\"" + issue.fields.summary +
+                    "\"";
+                issue_line.status = issue.fields.status.name;
+                issue_line.points = issue.fields.customfield_10003;
+                issue_line.projected_lead_time = issue.fields.customfield_10003 *
+                    GOJIRA.config.points_per_day;
+                populate_times(issue_line, durations, issue.key,
+                    GOJIRA.config.csv_header_columns);
 
-                for (var x = 0; x < issues.length; x++) {
-                    var issue = issues[x];
-                    var issue_line = function() {};
-                    issue_line.type = issue.fields.issuetype.name;
-                    issue_line.key = issue.key;
-                    issue_line.summary = "\"" + issue.fields.summary +
-                        "\"";
-                    issue_line.status = issue.fields.status.name;
-                    issue_line.points = issue.fields.customfield_10003;
-                    issue_line.projected_lead_time = issue.fields.customfield_10003 *
-                        GOJIRA.config.points_per_day;
-                    populate_times(issue_line, durations, issue.key,
-                        GOJIRA.config.csv_header_columns);
-
-                    csv += GOJIRA.csv.from(issue_line);
-                }
-
-                GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path,
-                    csv);
+                csv += GOJIRA.csv.from(issue_line);
             }
+
+            GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path,
+                csv);
         }
-    })
-);
+    }
+});
