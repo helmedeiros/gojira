f1a33aff425594d71d89975cb5ceb715b59e6b3f
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-11-24 12:02:26 -0200

    formatting code following previous pattern

--- index.js
+++ index.js
@@ -11,88 +11,91 @@ var _ = require("underscore");
 var day = 60 * 60 * 24 * 1000;
 
 var populate_times = function(line, issues, issue_key, csv_columns) {
-  var issue = _.find(issues, function(i) {
-    return i.key == issue_key;
-  });
-  if (issue) {
-    var wt = issue.workingTime;
+    var issue = _.find(issues, function(i) {
+        return i.key == issue_key;
+    });
+    if (issue) {
+        var wt = issue.workingTime;
 
-    var columns_size = csv_columns.split(',').length;
-    console.log('COLUMNS SIZE: ' + columns_size + '\n\n');
-    var time_line = '';
-    var lead_time = 0;
-    for (var columns = 0; columns < columns_size; columns++) {
-      if (columns < GOJIRA.config.first_column_to_count) {
-        time_line += parseInt((wt[columns] / day) * 10) / 10;
-      } else if (columns == columns_size - 1) {
-        time_line += parseInt((wt[columns] / day) * 10) / 10;
-      } else {
-        lead_time += parseInt((wt[columns] / day) * 10) / 10;
-        time_line += parseInt((wt[columns] / day) * 10) / 10;
-      }
-      time_line += ',';
+        var columns_size = csv_columns.split(',').length;
+        console.log('COLUMNS SIZE: ' + columns_size + '\n\n');
+        var time_line = '';
+        var lead_time = 0;
+        for (var columns = 0; columns < columns_size; columns++) {
+            if (columns < GOJIRA.config.first_column_to_count) {
+                time_line += parseInt((wt[columns] / day) * 10) / 10;
+            } else if (columns == columns_size - 1) {
+                time_line += parseInt((wt[columns] / day) * 10) / 10;
+            } else {
+                lead_time += parseInt((wt[columns] / day) * 10) / 10;
+                time_line += parseInt((wt[columns] / day) * 10) / 10;
+            }
+            time_line += ',';
 
+        }
+
+        //if (line.key === 'DEMO-905') {
+        //    line.in_progress = line.in_progress - 22;
+        //}
+        line.times = time_line;
+        line.lead_time = lead_time;
     }
-
-    //if (line.key === 'DEMO-905') {
-    //    line.in_progress = line.in_progress - 22;
-    //}
-    line.times = time_line;
-    line.lead_time = lead_time;
-  }
 };
 
 var durations = [];
 
 var issues_url = GOJIRA.url.issues(GOJIRA.config.project_key, GOJIRA.config.component,
-  GOJIRA.config.work_group, GOJIRA.config.max_results, GOJIRA.config.user,
-  GOJIRA.config.password);
+    GOJIRA.config.work_group, GOJIRA.config.max_results, GOJIRA.config.user,
+    GOJIRA.config.password);
 
 var control_chart_url = GOJIRA.url.control_chart(GOJIRA.config.control_chart,
-  GOJIRA.config.user,
-  GOJIRA.config.password, GOJIRA.config.from, GOJIRA.config.to);
+    GOJIRA.config.user,
+    GOJIRA.config.password, GOJIRA.config.from, GOJIRA.config.to);
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 request(control_chart_url, function(error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    var json = JSON.parse(body);
-    durations = json.issues;
-  }
-}).pipe(
-  request(issues_url, function(error, response, body) {
     if (error) {
-      console.log(error);
+        console.log(error);
     } else {
-      var json = JSON.parse(body);
-      var issues = json.issues;
-
-      if (!issues) {
-        console.error(
-          'No issues returned. Please check your project, component and work group settings.\n'
-        )
-      } else {
-
-        var csv = GOJIRA.csv.header(GOJIRA.config.csv_header_columns);
-
-        for (var x = 0; x < issues.length; x++) {
-          var issue = issues[x];
-          var issue_line = function() {};
-          issue_line.type = issue.fields.issuetype.name;
-          issue_line.key = issue.key;
-          issue_line.summary = "\"" + issue.fields.summary + "\"";
-          issue_line.status = issue.fields.status.name;
-          issue_line.points = issue.fields.customfield_10003;
-          issue_line.projected_lead_time = issue.fields.customfield_10003 *
-            GOJIRA.config.points_per_day;
-          populate_times(issue_line, durations, issue.key, GOJIRA.config.csv_header_columns);
-
-          csv += GOJIRA.csv.from(issue_line);
-        }
-
-        GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path, csv);
-      }
+        var json = JSON.parse(body);
+        durations = json.issues;
     }
-  })
+}).pipe(
+    request(issues_url, function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            var json = JSON.parse(body);
+            var issues = json.issues;
+
+            if (!issues) {
+                console.error(
+                    'No issues returned. Please check your project, component and work group settings.\n'
+                )
+            } else {
+
+                var csv = GOJIRA.csv.header(GOJIRA.config.csv_header_columns);
+
+                for (var x = 0; x < issues.length; x++) {
+                    var issue = issues[x];
+                    var issue_line = function() {};
+                    issue_line.type = issue.fields.issuetype.name;
+                    issue_line.key = issue.key;
+                    issue_line.summary = "\"" + issue.fields.summary +
+                        "\"";
+                    issue_line.status = issue.fields.status.name;
+                    issue_line.points = issue.fields.customfield_10003;
+                    issue_line.projected_lead_time = issue.fields.customfield_10003 *
+                        GOJIRA.config.points_per_day;
+                    populate_times(issue_line, durations, issue.key,
+                        GOJIRA.config.csv_header_columns);
+
+                    csv += GOJIRA.csv.from(issue_line);
+                }
+
+                GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path,
+                    csv);
+            }
+        }
+    })
 );
--- spec/url_spec.js
+++ spec/url_spec.js
@@ -1,55 +1,58 @@
 var url = require("../lib/url");
 
 describe("url", function() {
-  it(
-    "should return a issues url using project, component, work group, max results, and user",
-    function() {
-      var project = 'DEMO';
-      var component = 'Example Component';
-      var work_group = 'Application';
-      var max_results = '100';
-      var user = 'a_user';
-      var password = 'a_password';
+    it(
+        "should return a issues url using project, component, work group, max results, and user",
+        function() {
+            var project = 'DEMO';
+            var component = 'Example Component';
+            var work_group = 'Application';
+            var max_results = '100';
+            var user = 'a_user';
+            var password = 'a_password';
 
-      var generated_url = url.issues(project, component, work_group,
-        max_results, user, password);
+            var generated_url = url.issues(project, component,
+                work_group,
+                max_results, user, password);
 
-      var expected_url =
-        'https://jira.example.com/rest/api/2/search?jql=project=DEMO+and+status=Done+and+component="Example Component"+and+"Work Group"="Application"+and+type=Story&maxResults=100&os_username=a_user&os_password=a_password';
-      expect(generated_url).toBe(expected_url);
-    });
+            var expected_url =
+                'https://jira.example.com/rest/api/2/search?jql=project=DEMO+and+status=Done+and+component="Example Component"+and+"Work Group"="Application"+and+type=Story&maxResults=100&os_username=a_user&os_password=a_password';
+            expect(generated_url).toBe(expected_url);
+        });
 
-  it(
-    "should return a issues url using only project, default to 300 of max results, and user",
-    function() {
-      var project = 'DEMO';
-      var component = '';
-      var user = 'a_user';
-      var password = 'a_password';
+    it(
+        "should return a issues url using only project, default to 300 of max results, and user",
+        function() {
+            var project = 'DEMO';
+            var component = '';
+            var user = 'a_user';
+            var password = 'a_password';
 
-      var generated_url = url.issues(project, component, null, null, user,
-        password);
+            var generated_url = url.issues(project, component, null,
+                null, user,
+                password);
 
-      var expected_url =
-        'https://jira.example.com/rest/api/2/search?jql=project=DEMO+and+status=Done+and+type=Story&maxResults=300&os_username=a_user&os_password=a_password';
-      expect(generated_url).toBe(expected_url);
-    });
+            var expected_url =
+                'https://jira.example.com/rest/api/2/search?jql=project=DEMO+and+status=Done+and+type=Story&maxResults=300&os_username=a_user&os_password=a_password';
+            expect(generated_url).toBe(expected_url);
+        });
 
-  it(
-    "should build the json control chart url based on http control chart url, with user and dates",
-    function() {
-      var control_chart =
-        'https://jira.example.com/secure/RapidBoard.jspa?rapidView=1853&view=reporting&chart=controlChart&swimlane=12000&swimlane=10450&swimlane=11466&swimlane=11263&swimlane=11138&swimlane=10926&swimlane=7060&swimlane=7764&swimlane=7062&swimlane=7061&swimlane=7058&column=4325&column=4328&column=4334&days=7';
-      var user = 'a_user';
-      var password = 'a_password';
-      var from = '2015-01-01';
-      var to = '2015-12-31';
+    it(
+        "should build the json control chart url based on http control chart url, with user and dates",
+        function() {
+            var control_chart =
+                'https://jira.example.com/secure/RapidBoard.jspa?rapidView=1853&view=reporting&chart=controlChart&swimlane=12000&swimlane=10450&swimlane=11466&swimlane=11263&swimlane=11138&swimlane=10926&swimlane=7060&swimlane=7764&swimlane=7062&swimlane=7061&swimlane=7058&column=4325&column=4328&column=4334&days=7';
+            var user = 'a_user';
+            var password = 'a_password';
+            var from = '2015-01-01';
+            var to = '2015-12-31';
 
-      var generated_url = url.control_chart(control_chart, user, password,
-        from, to);
+            var generated_url = url.control_chart(control_chart,
+                user, password,
+                from, to);
 
-      var expected_url =
-        'https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=12000&swimlaneId=10450&swimlaneId=11466&swimlaneId=11263&swimlaneId=11138&swimlaneId=10926&swimlaneId=7060&swimlaneId=7764&swimlaneId=7062&swimlaneId=7061&swimlaneId=7058&from=2015-01-01&to=2015-12-31&os_username=a_user&os_password=a_password';
-      expect(generated_url).toBe(expected_url)
-    });
+            var expected_url =
+                'https://jira.example.com/rest/greenhopper/1.0/rapid/charts/controlchart?rapidViewId=1853&swimlaneId=12000&swimlaneId=10450&swimlaneId=11466&swimlaneId=11263&swimlaneId=11138&swimlaneId=10926&swimlaneId=7060&swimlaneId=7764&swimlaneId=7062&swimlaneId=7061&swimlaneId=7058&from=2015-01-01&to=2015-12-31&os_username=a_user&os_password=a_password';
+            expect(generated_url).toBe(expected_url)
+        });
 });
