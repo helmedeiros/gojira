c2c83b31edcf7c083f61858eeffc7f3d3680b10d
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-12-14 09:48:00 -0200

    test(auth_query): cover user_url builder

---
+++ spec/auth_query_spec.js
@@ -0,0 +1,13 @@
+var auth_query = require('../lib/auth_query');
+
+describe('auth_query', function () {
+    it('returns empty when user is missing', function () {
+        expect(auth_query.user_url('', 'p')).toBe('');
+        expect(auth_query.user_url(null, 'p')).toBe('');
+    });
+
+    it('builds username and password query', function () {
+        expect(auth_query.user_url('a_user', 'a_password'))
+            .toBe('&os_username=a_user&os_password=a_password');
+    });
+});
