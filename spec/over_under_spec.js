bf58ceb699c226933c2756ac90c8679606555fa9
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2015-12-22 14:18:00 -0200

    test(over_under): cover diff and ratio

---
+++ spec/over_under_spec.js
@@ -0,0 +1,31 @@
+var over_under = require('../lib/over_under');
+
+describe('over_under', function () {
+    describe('diff', function () {
+        it('is zero when actual equals projected', function () {
+            expect(over_under.diff(5, 5)).toBe(0);
+        });
+
+        it('is positive when actual exceeds projected', function () {
+            expect(over_under.diff(8, 5)).toBe(3);
+        });
+
+        it('is negative when actual is under projected', function () {
+            expect(over_under.diff(3, 5)).toBe(-2);
+        });
+    });
+
+    describe('ratio', function () {
+        it('is 1 when actual equals projected', function () {
+            expect(over_under.ratio(5, 5)).toBe(1);
+        });
+
+        it('is greater than 1 when actual exceeds projected', function () {
+            expect(over_under.ratio(8, 5)).toBe(1.6);
+        });
+
+        it('is less than 1 when actual is under projected', function () {
+            expect(over_under.ratio(3, 5)).toBe(0.6);
+        });
+    });
+});
