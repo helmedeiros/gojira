5ff4a101dbd7b7de4b235028531344855d629ce4
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-01-04 14:21:00 -0200

    test(issue_line): cover mapping and projection

---
+++ spec/issue_line_spec.js
@@ -0,0 +1,35 @@
+var issue_line = require('../lib/issue_line');
+
+describe('issue_line', function () {
+    var jira_issue;
+
+    beforeEach(function () {
+        jira_issue = {
+            key: 'DEMO-1',
+            fields: {
+                issuetype: { name: 'Story' },
+                status: { name: 'Done' },
+                summary: 'Sample Story',
+                customfield_10003: 4
+            }
+        };
+    });
+
+    it('maps type, key, status and quoted summary', function () {
+        var line = issue_line.from(jira_issue, 1.25);
+        expect(line.type).toBe('Story');
+        expect(line.key).toBe('DEMO-1');
+        expect(line.status).toBe('Done');
+        expect(line.summary).toBe('"Sample Story"');
+    });
+
+    it('exposes story points', function () {
+        var line = issue_line.from(jira_issue, 1.25);
+        expect(line.points).toBe(4);
+    });
+
+    it('projects lead time from points and points_per_day', function () {
+        var line = issue_line.from(jira_issue, 1.25);
+        expect(line.projected_lead_time).toBe(5);
+    });
+});
