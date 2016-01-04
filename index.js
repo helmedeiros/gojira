adbab91d1c03d1574d8f6624dde5a63caa476439
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-01-04 09:17:00 -0200

    refactor(index): use durations module

--- index.js
+++ index.js
@@ -4,43 +4,9 @@ GOJIRA.config = require('./lib/config').load();
 GOJIRA.util = require('./lib/util');
 GOJIRA.csv = require('./lib/csv');
 GOJIRA.url = require('./lib/url');
+GOJIRA.durations = require('./lib/durations');
 
 var request = require("request");
-var _ = require("underscore");
-
-var day = 60 * 60 * 24 * 1000;
-
-var populate_times = function(line, issues, issue_key, csv_columns) {
-    var issue = _.find(issues, function(i) {
-        return i.key == issue_key;
-    });
-    if (issue) {
-        var wt = issue.workingTime;
-
-        var columns_size = csv_columns.split(',').length;
-        console.log('COLUMNS SIZE: ' + columns_size + '\n\n');
-        var time_line = '';
-        var lead_time = 0;
-        for (var columns = 0; columns < columns_size; columns++) {
-            if (columns < GOJIRA.config.first_column_to_count) {
-                time_line += parseInt((wt[columns] / day) * 10) / 10;
-            } else if (columns == columns_size - 1) {
-                time_line += parseInt((wt[columns] / day) * 10) / 10;
-            } else {
-                lead_time += parseInt((wt[columns] / day) * 10) / 10;
-                time_line += parseInt((wt[columns] / day) * 10) / 10;
-            }
-            time_line += ',';
-
-        }
-
-        //if (line.key === 'DEMO-905') {
-        //    line.in_progress = line.in_progress - 22;
-        //}
-        line.times = time_line;
-        line.lead_time = lead_time;
-    }
-};
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -92,8 +58,9 @@ request(issues_url, function(error, response, body) {
                 issue_line.points = issue.fields.customfield_10003;
                 issue_line.projected_lead_time = issue.fields.customfield_10003 *
                     GOJIRA.config.points_per_day;
-                populate_times(issue_line, durations, issue.key,
-                    GOJIRA.config.csv_header_columns);
+                GOJIRA.durations.populate(issue_line, durations, issue.key,
+                    GOJIRA.config.csv_header_columns,
+                    GOJIRA.config.first_column_to_count);
 
                 csv += GOJIRA.csv.from(issue_line);
             }
