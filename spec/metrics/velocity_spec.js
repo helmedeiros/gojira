75acb17e48e0d41c5a993036e9493ea4fbcc2071
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-01-26 16:27:00 -0200

    test(metrics): cover velocity sum

---
+++ spec/metrics/velocity_spec.js
@@ -0,0 +1,19 @@
+var velocity = require('../../lib/metrics/velocity');
+
+describe('velocity', function () {
+    it('returns 0 for missing input', function () {
+        expect(velocity.compute(null)).toBe(0);
+    });
+
+    it('returns 0 for empty array', function () {
+        expect(velocity.compute([])).toBe(0);
+    });
+
+    it('sums points across lines', function () {
+        expect(velocity.compute([{ points: 3 }, { points: 5 }, { points: 8 }])).toBe(16);
+    });
+
+    it('treats missing points as zero', function () {
+        expect(velocity.compute([{ points: 3 }, {}, { points: 2 }])).toBe(5);
+    });
+});
