e8d26e2505d447244e159ed8855266e8de6dfc9b
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-01-28 12:31:00 -0200

    test(metrics): cover wip filter

---
+++ spec/metrics/wip_spec.js
@@ -0,0 +1,22 @@
+var wip = require('../../lib/metrics/wip');
+
+describe('wip', function () {
+    it('returns 0 for missing input', function () {
+        expect(wip.compute(null)).toBe(0);
+    });
+
+    it('returns 0 when all items are Backlog or Done', function () {
+        var lines = [{ status: 'Backlog' }, { status: 'Done' }, { status: 'Done' }];
+        expect(wip.compute(lines)).toBe(0);
+    });
+
+    it('counts items in active statuses', function () {
+        var lines = [
+            { status: 'Backlog' },
+            { status: 'In Progress' },
+            { status: 'Validation' },
+            { status: 'Done' }
+        ];
+        expect(wip.compute(lines)).toBe(2);
+    });
+});
