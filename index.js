b66c83ffd5a3b092294c9868eeb638d6852a0f17
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-02-05 11:23:00 -0200

    feat(output): support stdout target

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ require('./lib/http').configure({ timeout: GOJIRA.config.request_timeout_ms });
 GOJIRA.extract = require('./lib/extract');
 
 GOJIRA.extract.run(GOJIRA.config)
-    .then(function (csv) {
-        if (!csv) {
+    .then(function (output) {
+        if (!output) {
             console.error('No issues returned. Please check your project, component and work group settings.\n');
             return;
         }
-        return GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path, csv);
+        if (GOJIRA.config.output_target === 'stdout') {
+            console.log(output);
+            return;
+        }
+        return GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path, output);
     })
     .catch(function (error) {
         console.log(error);
