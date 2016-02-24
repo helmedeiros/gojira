2a912862f5ee46e0375e9bcfb5d0f3ec0d2fe76d
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-02-24 09:32:00 -0200

    refactor(main): extract testable entry point

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-var GOJIRA = GOJIRA || {};
+var main = require('./lib/main');
 
-GOJIRA.config = require('./lib/config').load();
-GOJIRA.util = require('./lib/util');
-require('./lib/http').configure({ timeout: GOJIRA.config.request_timeout_ms });
-GOJIRA.extract = require('./lib/extract');
-
-GOJIRA.extract.run(GOJIRA.config)
-    .then(function (output) {
-        if (!output) {
-            console.error('No issues returned. Please check your project, component and work group settings.\n');
-            return;
-        }
-        if (GOJIRA.config.output_target === 'stdout') {
-            console.log(output);
-            return;
-        }
-        return GOJIRA.util.save_to_file(GOJIRA.config.output_csv_path, output);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
+main.run().catch(function (error) {
+    console.log(error);
+});
