0951f202671a6e738fca28519e0e60b84150a460
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-03-16 11:14:00 -0200

    refactor(index): dual-mode entry (export when required, run when invoked)

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var cli = require('./lib/cli');
 var main = require('./lib/main');
+var cli = require('./lib/cli');
 var logger = require('./lib/logger');
 
-main.run(cli.parse(process.argv)).catch(function (error) {
-    logger.error(error.message || error);
-    process.exit(1);
-});
+module.exports = {
+    run: main.run
+};
+
+if (require.main === module) {
+    main.run(cli.parse(process.argv)).catch(function (error) {
+        logger.error(error.message || error);
+        process.exit(1);
+    });
+}
