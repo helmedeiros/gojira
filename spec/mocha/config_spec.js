c6eb71775f00e55c24d8d364d8e3696d6d12c561
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-04-29 16:24:00 -0200

    feat(config): add include_changelog flag (default false)

--- spec/mocha/config_spec.js
+++ spec/mocha/config_spec.js
@@ -73,6 +73,11 @@ describe('config', function () {
         expect(loaded.include_metrics).to.equal(false);
     });
 
+    it('defaults include_changelog to false when missing', function () {
+        var loaded = config.load('./spec/fixtures/sample_config.json');
+        expect(loaded.include_changelog).to.equal(false);
+    });
+
     it('defaults output_format to csv when missing', function () {
         var loaded = config.load('./spec/fixtures/sample_config.json');
         expect(loaded.output_format).to.equal('csv');
