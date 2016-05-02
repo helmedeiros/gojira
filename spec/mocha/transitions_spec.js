aecc63c4024a86985884bd6d7854c73ab89ce0b9
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2016-05-02 16:24:00 -0200

    test(transitions): cover real-event extraction and back-derive fallback

--- spec/mocha/transitions_spec.js
+++ spec/mocha/transitions_spec.js
@@ -54,3 +54,68 @@ describe('transitions.for_lines', function () {
         expect(transitions.for_lines([])).to.eql([]);
     });
 });
+
+describe('transitions.for_line with real changelog events', function () {
+    var columns = ['Backlog', 'In Progress', 'Done'];
+
+    it('uses real transition timestamps when all columns are matched', function () {
+        var line = {
+            resolved_at: '2016-02-01T00:00:00Z',
+            times_array: [1, 5, 0],
+            transitions: [
+                { at: '2016-01-10T09:00:00Z', to_status: 'Backlog' },
+                { at: '2016-01-12T09:00:00Z', to_status: 'In Progress' },
+                { at: '2016-01-15T09:00:00Z', to_status: 'Done' }
+            ]
+        };
+        var entered = transitions.for_line(line, columns);
+        expect(entered[0]).to.equal(Date.parse('2016-01-10T09:00:00Z'));
+        expect(entered[1]).to.equal(Date.parse('2016-01-12T09:00:00Z'));
+        expect(entered[2]).to.equal(Date.parse('2016-01-15T09:00:00Z'));
+    });
+
+    it('takes the FIRST event when a column is entered multiple times', function () {
+        var line = {
+            transitions: [
+                { at: '2016-01-10T00:00:00Z', to_status: 'Backlog' },
+                { at: '2016-01-12T00:00:00Z', to_status: 'In Progress' },
+                { at: '2016-01-13T00:00:00Z', to_status: 'Backlog' },
+                { at: '2016-01-14T00:00:00Z', to_status: 'In Progress' },
+                { at: '2016-01-15T00:00:00Z', to_status: 'Done' }
+            ]
+        };
+        var entered = transitions.for_line(line, columns);
+        expect(entered[1]).to.equal(Date.parse('2016-01-12T00:00:00Z'));
+    });
+
+    it('falls back to back-derivation when a column has no matching event', function () {
+        var resolved = Date.UTC(2016, 0, 20);
+        var DAY = 24 * 60 * 60 * 1000;
+        var line = {
+            resolved_at: new Date(resolved).toISOString(),
+            times_array: [2, 3, 0],
+            transitions: [
+                { at: '2016-01-10T00:00:00Z', to_status: 'Backlog' }
+            ]
+        };
+        var entered = transitions.for_line(line, columns);
+        expect(entered[2]).to.equal(resolved);
+        expect(entered[1]).to.equal(resolved - 3 * DAY);
+        expect(entered[0]).to.equal(resolved - 5 * DAY);
+    });
+
+    it('falls back when columns are not supplied', function () {
+        var resolved = Date.UTC(2016, 0, 20);
+        var line = {
+            resolved_at: new Date(resolved).toISOString(),
+            times_array: [1, 1, 0],
+            transitions: [
+                { at: '2016-01-10T00:00:00Z', to_status: 'Backlog' },
+                { at: '2016-01-12T00:00:00Z', to_status: 'In Progress' },
+                { at: '2016-01-15T00:00:00Z', to_status: 'Done' }
+            ]
+        };
+        var entered = transitions.for_line(line);
+        expect(entered[2]).to.equal(resolved);
+    });
+});
